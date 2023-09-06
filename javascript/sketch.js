7d5ec0a7504c062f547f02ed9be8abd930572c46
safnawaz <snawa003@gold.ac.uk>
2023-09-06 11:16:10 +0100

    batch
    -bg changes based on valence
    -added home page and linked

--- javascript/sketch.js
+++ javascript/sketch.js
@@ -14,9 +14,9 @@ function setup() {
   soundFormats('wav');
   song = loadSound('sound/Cut_2011_1.wav');
 
-  button = createButton('next memory');
-  button.position(40, 55);
-  button.mousePressed(nextMem);
+  // button = createButton('next memory');
+  // button.position(30, 55);
+  // button.mousePressed(nextMem);
 
   fetch("./json/memories2.json").then(function(response) {
     return response.json();
@@ -35,38 +35,61 @@ function setup() {
 }
 
 function draw() {
- 
+  let xMap = map(mouseX,0,width,0,100);
+  let yMap = map(mouseY,0,height,0,100);
+
   let temporarymemory = memories[index];
   let tmenergy = temporarymemory.energy;
-  let tmvalence = temporarymemory.valence;
   let tmvividness = temporarymemory.vivid;
-  let color = map(tmvalence,1,5,0,175);
   let size = map(tmvividness,1,5,0,400);
-  let increment = map(tmenergy,1,5,0.005,0.03);
+  let increment = map(tmenergy,1,5,0.003,0.01);
   let tmsong = temporarymemory.songname;
   let tmartist = temporarymemory.artist;
   let tmmemory = temporarymemory.description;
-  let love = 'love';
-  let m = match(tmmemory,love);
 
-  if (m == 'love'){
-    background(240,20,90);
+
+  let tmvalence = temporarymemory.valence;
+  let color;
+  color = map(tmvalence,1,5,0,255);
+
+  if (tmvalence <=3){
+    background(xMap,yMap,255);
+
   } else {
-    background(100,83,210,200);
+    background(255,xMap,yMap);
+
   }
+  
+  
+// making a change if word 'love' is detected in memory description
+// let love = 'love';
+// let m = match(tmmemory,love);  
+// if (m == 'love'){
+  //   background(240,20,90);
+  // } else {
+  //   background(100,83,210,200);
+  // }
 
   textSize(60);
   fill(255,255,255,40);
   text(tmmemory,20,70,width-40,600);
   
+  if (tmvalence <=3){
 
+    stroke(xMap,yMap,200);
+  } else {
+  
+    stroke(200,xMap,yMap);
+  }
   
 
   translate(width/2,height/2);
-  noStroke();
-  fill(255,color,0);
+  
+  fill(255,150);
+  strokeWeight(20);
   pulse = sin(frame)*size;
   ellipse(0,0,pulse,pulse);
+  noStroke();
   
  translate(-width/2,-height/2);
  textSize(20);
@@ -83,6 +106,10 @@ function draw() {
   frame = frame + increment;
   
  
+}
+
+function valenceChange(){
+  
 }
 
 function keyPressed(){
@@ -112,50 +139,4 @@ function keyPressed(){
 //   song.play();
 // }
 
-function drawChart(){
-  
- 
-  
-  
-  // let total = 0; 
-  // for (let i= 0 ; i<breakfast.length; i++) {
-  //   total += breakfast[i].amount;
-  // }
 
-  // let centreX = width/2;
-  // let centreY = height/2; 
-  // let diam = 300;
-  // let angleStart = TWO_PI*0.75; 
-
-  // for (let i=0; i<breakfast.length; i++) {
-
-  //   let item = breakfast[i];
-
-  //   let itemFraction = item.amount/total;
-  //   let itemAngle = itemFraction * TWO_PI; 
-  //   let angleEnd = angleStart + itemAngle;
-
-  //   //normal pie
-  //   fill(item.color);
-  //   stroke(0, 0, 0); 
-  //   strokeWeight(1); 
-  //   strokeJoin(ROUND); 
-  //   arc(centreX, centreY, diam, diam, angleStart, angleEnd, PIE); //PIE creates closed slices the the center
-
-
-  //   noStroke();
-  //   fill(0); 
-  //   push();
-  //   translate(centreX, centreY); 
-  //   rotate(angleEnd); 
-  //   textAlign(RIGHT, BOTTOM); 
-  //   //normal pie
-  //   text(item.ingredient, diam/2 - 20, -8); 
-
-  //   pop();
-
-  //   //update the angle start before the next iteration
-  //   angleStart += itemAngle;
-  // }
-
-}
